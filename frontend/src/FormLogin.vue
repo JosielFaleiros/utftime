a29027135d52fdb82b9d4fee42589e70cdce3536
JosielFaleiros <josiel.2015@alunos.utfpr.edu.br>
2017-09-30 21:10:41 -0300

    Componetizado formulário de login

---
+++ frontend/src/FormLogin.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container-fluid text-center">
+        <div class="row content">
+            <div class="col-sm-8 text-left">
+                <div class="container-fluid col-md-offset-6 col-centered">
+                    <h2>Login Aluno</h2>
+                    <hr>
+                    <div class="form-group">
+                        <label for="sel1">RA:</label>
+                        <div class="input-group ra">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                            <input type="text" class="form-control" id="sel1" placeholder="Somente números">
+                        </div>
+
+                    </div>
+                    <div class="form-group">
+                        <label for="sel2">Senha:</label>
+                        <div class="input-group senha">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                            <input type="password" class="form-control" id="sel5" minlength="4" maxlength="8" size="8" placeholder="até 8 digitos">
+                        </div>
+                    </div>
+                    <p>Se você não tem uma conta, crie uma <a href="#">aqui</a>
+                    </p>
+                    <div class="form-group">
+                        <button type="submit" class="btn mybtn btn-lg">Entrar</button>
+                    </div>
+                    <hr>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  name: 'formlogin'
+}
+</script>
--- frontend/src/login.js
+++ frontend/src/login.js
@@ -1,15 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './FormLogin'
 import router from './router'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: '#applogin',
   router,
-  template: '<App/>',
-  components: { App }
+  render: h => h(App)
 })
