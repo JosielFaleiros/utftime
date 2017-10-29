06b5013c051bac9401fbe945f131c81fe895e08b
guimpo <nakaima.prk@gmail.com>
2017-10-29 04:38:39 -0200

    Adicionado relacionamento

---
+++ app/migrations/20171029063749-change-item.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.sequelize.query("ALTER TABLE Item ADD CONSTRAINT grupoId_fk FOREIGN KEY (grupoId) REFERENCES Grupo(id);")
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.sequelize.query("ALTER TABLE Item DROP CONSTRAINT grupoId_fk;")
+  }
+};
