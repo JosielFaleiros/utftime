c2f195b19a4ad8fbdf8d2f73da9be515db52e38b
guimpo <nakaima.prk@gmail.com>
2017-10-29 02:42:30 -0200

    Corrigido migrations

---
+++ app/migrations/20171029043612-create-grupo.js
@@ -0,0 +1,27 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Grupo', {
+      idGrupo: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nome: {
+        type: Sequelize.STRING(50)
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Grupo');
+  }
+};
