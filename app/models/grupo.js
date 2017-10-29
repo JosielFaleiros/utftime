cd31db63000a4b3c901a36356054d65893ec8b3c
guimpo <nakaima.prk@gmail.com>
2017-10-29 02:39:37 -0200

    Adicionado modelo: Grupo

---
+++ app/models/grupo.js
@@ -0,0 +1,21 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  let Grupo = sequelize.define('Grupo', {
+    idGrupo: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    nome: {
+      type: DataTypes.STRING(50)
+    }
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Grupo;
+};
