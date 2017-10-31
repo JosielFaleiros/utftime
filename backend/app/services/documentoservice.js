622918166156c80562695a6c59c12ee02a85310e
JosielFaleiros <josiel.2015@alunos.utfpr.edu.br>
2017-10-31 15:03:25 -0200

    GET /api/documento implementado

--- backend/app/services/documentoservice.js
+++ backend/app/services/documentoservice.js
@@ -1,9 +1,38 @@
 const path = require('path')
 const models = require('../models')
 
-function findAll(req, res, next) {
-  // TODO: create the service for this
-  res.status(200).send('{ documentos: [ { iddocumento: "0", url: "string", grupo_idgrupo: "0", usuario_idusuario: "0", status_idstatus: "0", pontuacao: "0" } ], grupos: [ { idgrupo: "0", nome: "string", descricao: "string" } ], status: [ { idstatus: "0", descricao: "string" } ] }')
+async function findAll(req, res, next) {
+  let user = await models.Usuario.find({where: {idusuario: req.token.idusuario}, include: [models.Papel]})
+  let papel = 0
+  if(user) papel = user.Papel.idpapel
+  switch (papel) {
+    case 1:/*CASO SEJA PROFESSOR*/
+      res.status(200).send({
+        documentos: await models.Documento.findAll({
+          include: [{model: models.Grupo}, {model: models.Usuario, attributes: ['idusuario', 'nome']}, {model: models.Status}]
+        }),
+        grupos: await models.Grupo.findAll(),
+        status: await models.Status.findAll()
+      })
+      break;
+    case 2:/*CASO SEJA ALUNO*/
+      res.status(200).send({
+        documentos: await models.Documento.findAll({
+          where: {usuario_idusuario: req.token.idusuario},
+          include: [{model: models.Grupo}, {model: models.Usuario, attributes: ['idusuario', 'nome']}, {model: models.Status}]
+        }),
+        grupos: await models.Grupo.findAll(),
+        status: await models.Status.findAll()
+      })
+      break;
+    default:
+      res.status(200).send({
+        documentos: [],
+        grupos: await models.Grupo.findAll(),
+        status: await models.Status.findAll()
+      })
+      break;
+  }
 }
 
 function find(req, res, next) {}
