c5ab435f6ce69aab7636a3b8271b8c520c1bf131
JosielFaleiros <josiel.2015@alunos.utfpr.edu.br>
2017-11-05 16:50:45 -0200

    PUT /api/documento funcionando, atualizando documentos

--- backend/app/services/documentoservice.js
+++ backend/app/services/documentoservice.js
@@ -44,24 +44,7 @@ function find(req, res, next) {}
 
 async function create(req, res, next) {
   try {
-    /*START SENDING FILE TO DISK*/
-    let matches_file = req.body.arquivo.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/)
-    if (matches_file.length !== 3) {
-      return new Error('Invalid input string');
-    }
-    let extenssion_file = matches_file[1].match(/\/(.*?)$/) // this is to take any extenssion format
-    let crypto = require("crypto")
-    let newfilename = crypto.randomBytes(8).toString('hex')
-    let image_name_path = config.files_path+newfilename+'.'+extenssion_file[1]
-    let url = newfilename+'.'+extenssion_file[1]
-    await base64.decode(matches_file[2], image_name_path, function(err, output) {
-      console.log(output)
-      if (err) {
-        throw new Error()
-      }
-    })
-    /*END SENDING FILE TO DISK*/
-
+    let url = await sendFileToDisk(req.body.arquivo)
 
     let newdoc = await models.Documento.create({
       nomearquivo: req.body.nomearquivo,
@@ -76,12 +59,63 @@ async function create(req, res, next) {
   }
 }
 
-function update(req, res, next) {
-  // TODO: create the service for this
-  // res.status(200).send('{ mensagem: "Documento não pode ser atualizado com estes dados."}')
-  res.status(204).send()
+async function update(req, res, next) {
+  try {
+    delete req.body.usuario_idusuario // o dono não pode ser alterado por ninguém
+    delete req.body.created_at// estes campos são automáticos
+    delete req.body.updated_at
+    delete req.body.url
+
+    // console.log(req.body);
+    let docf = await models.Documento.find({ where: { iddocumento: req.body.iddocumento } })
+    if (docf.usuario_idusuario == req.token.idusuario) {
+      // é o dono do documento, então não pode alterar a pontuação nem observação.
+      delete req.body.pontuacao
+      delete req.body.observacao
+    } else if (req.token.usuariopapel != '1') {
+      //não é professor, e nem o dono do documento
+      // TODO: throw authorization exception here with casl
+      throw new Error('not authorized user ;)')
+    }
+    if (req.body.arquivo) {
+      //se houver um novo arquivo, então entrará aqui, e um novo será adicionado
+      // TODO: remover o arquivo anterior do sistema, por ter sido rejeitado pela professora
+      req.body.url = await sendFileToDisk(req.body.arquivo)
+    }
+    // console.log(req.body)
+
+    let result = await models.Documento.update(
+      req.body,
+      { where: { iddocumento: req.body.iddocumento } }
+    )
+    console.log(result)
+    res.status(204).send({ mensagem: "Documento atualizado com sucesso."})
+  } catch (e) {
+    res.status(200).send({ mensagem: "Documento não pode ser atualizado com estes dados."})
+  }
 }
 
 function destroy(req, res, next) {}
+
+async function sendFileToDisk(arquivo) {
+  /*START SENDING FILE TO DISK*/
+  let matches_file = arquivo.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/)
+  if (matches_file.length !== 3) {
+    return new Error('Invalid input string')
+  }
+  let extenssion_file = matches_file[1].match(/\/(.*?)$/) // this is to take any extenssion format
+  let crypto = require("crypto")
+  let newfilename = crypto.randomBytes(8).toString('hex')
+  let image_name_path = config.files_path+newfilename+'.'+extenssion_file[1]
+  let url = newfilename+'.'+extenssion_file[1]
+  await base64.decode(matches_file[2], image_name_path, function(err, output) {
+    console.log(output)
+    if (err) {
+      throw new Error()
+    }
+  })
+  /*END SENDING FILE TO DISK*/
+  return url
+}
 
 module.exports = {findAll, find, create, update, destroy}
