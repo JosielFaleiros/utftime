d57d2ca2fad1bed44b7b7441aa6606355527ad9a
Lucas Joaquim <ljsatiro@gmail.com>
2017-11-22 01:37:55 -0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(__dirname + '/pages/js'));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/pages/index.html'));
+});
+
+app.get('/login', function (req, res) {
+    res.sendFile(path.join(__dirname + '/pages/Login.html'))
+});
+
+app.get('/cadastro', function (req, res) {
+    res.sendFile(path.join(__dirname + '/pages/Cadastro.html'))
+});
+
+app.listen(3000, function () {
+  console.log('Running in port 3000!');
+});
