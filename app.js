497522ad164b1b8278bebee04b260065550be41d
JamalSatiro <ljsatiro@gmail.com>
2017-11-22 11:15:37 -0200

    problema com arquivos estaticos

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use(express.static(__dirname + '/pages/js'));
+app.use(express.static(path.join(__dirname, '/pages/js')));
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/pages/index.html'));
