2ca29d2066732b82232dabe927715b7a06f95da1
Rodrigo Rolim <rodriggit config --global user.name Rodrigo>
2017-11-24 18:53:41 -0200

    script do banco de dados

--- scrip_bd.sql
+++ scrip_bd.sql
@@ -1,10 +1,10 @@
 create database if not exists  utftime;
 SET FOREIGN_KEY_CHECKS=0;
 drop table if exists utftime.curso;
-create table utftime.curso (
-	id_curso int primary key auto_increment,
-	nome_curso varchar(45) not null
-)engine=InnoDB;
+create table if not exists utftime.curso(
+id_curso	int primary key auto_increment,
+nome_curso	varchar(100) not null
+);
 drop table if exists utftime.usuario;
 create table if not exists utftime.usuario (
 nome 		varchar(155) not null,
@@ -13,13 +13,7 @@ senha		int(11) not null,
 curso		int not null,
 foreign key (curso) references curso(id_curso)
 )engine=InnoDB;
-drop table if exists utftime.grupo;
-create table if not exists utftime.grupo (
 
-id_grupo	int primary key auto_increment,
-nome_grupo	varchar(145) not null
-
-)engine=InnoDB;
 drop table if exists utftime.documento;
 create table if not exists utftime.documento (
 
@@ -27,10 +21,9 @@ id_documento  	int primary key auto_increment,
 email			varchar(100) not null,
 dono 			varchar(45) not null,
 pontuacao		int not null  default 0,
-estado			varchar(45),
-grupo			int not null,
+estado			varchar(45) not null,
+grupo			varchar(45) not null,
 foreign key (email) references usuario(email),
-foreign key (grupo) references grupo(id_grupo),
 check (estado in ( 'aprovado', 'devolvido', 'em análise'))
 )engine=InnoDB;
 SET FOREIGN_KEY_CHECKS=1;
