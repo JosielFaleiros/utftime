47e55b499d8bbde3eaecef2a59a4e533b99e2365
JamalSatiro <ljsatiro@gmail.com>
2017-11-27 13:15:13 -0200

    bug fixed

--- pages/js/cadastro.js
+++ pages/js/cadastro.js
@@ -2,19 +2,25 @@ $(document).ready(function(){
 
     $('#enviar').click(function(){
 
-        if( $('#sel5').val() != $('#sel6').val() ){
-            alert('Senha digitada errada!');
-            return;
-        }
-
         let campos = $('label').text().split(':');
         let tamanho = campos.length - 1;
         for(let i = 0; i < tamanho; ++i){
-            if( $(`input[name=sel${i}]`).val() == '' ){
-                alert(`Campo ${campos[i]} está vazio`);
+            if( $(`input[name=sel${i+1}]`).val() == '' ){
+                alert(`Campo ${campos[i].toLowerCase()} está vazio`);
                 return;
             }
         }
+
+        if( !/^[a-z0-9]*@alunos\.utfpr\.edu\.br$/.test( $("input[name=sel4]").val()) ){
+            alert(`Email inválido!`);
+            return;
+        }
+        
+        if( $('#sel5').val() != $('#sel6').val() ){
+            alert('Repetição de senha inválida!');
+            return;
+        }
+
         $.ajax({
             method: "POST",
             url: "/cadastro",
